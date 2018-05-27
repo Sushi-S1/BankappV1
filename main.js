46c77d7e609ba1e7903016f0024f48cbe710ceb8
Sci-Phy <tristanlusardi@gmail.com>
2018-05-27 01:49:23 -0400

    Added code to add new users to the database Rearranged some stuff in main.js Removed redundant buttons in transHist.html modals Renamed data2.json to database.json

--- main.js
+++ main.js
@@ -8,37 +8,47 @@ var config = {
 };
 firebase.initializeApp(config);
 
-$(function () {
+$(function ()
+{
   $('[data-toggle="popover"]').popover()
 })
 
-function validatedropdown(){
+function validatedropdown()
+{
 
 }
-function signout(){
+function signout()
+{
   window.location.assign("index.html")
 }
-function loginOpen(){
-
-
+function loginOpen()
+{
   $("#loginmodal").modal();
-
 }
 
-function paydebtopen(){
+function paydebtopen()
+{
   $("#paydebt").modal();
 }
 
-function getloanopen(){
+function getloanopen()
+{
   $("#loanmodal").modal();
 }
 
-
-function createaccountopen(){
-  $("#createmodal").modal();
+function clearLOGIN()
+{
+  document.getElementById("username1").value = "";
+  document.getElementById("password").value = "";
 }
 
-function clearCreate(){
+function createaccountopen()
+{
+  $("#createmodal").modal('toggle');
+}
+
+function clearCreate()
+{
   document.getElementById("createnamefirst").value = "";
   document.getElementById("createnamelast").value = "";
   document.getElementById("createusername").value = "";
@@ -47,86 +57,104 @@ function clearCreate(){
   document.getElementById("createpass2").value = "";
 }
 
-function clearLOGIN(){
+function createUSER(first, last, username, email, password)
+{
+    firebase.database().ref('UserINFO/' + first + '/').set({
+      First: first,
+      Last: last,
+      Username: username,
+      Email: email,
+      Password: password
+    });
+}
+
+function SignUp()
+{
+    var first = document.getElementById("createnamefirst").value;
+    var last = document.getElementById("createnamelast").value;
+    var username = document.getElementById("createusername").value;
+    var email = document.getElementById("createemail").value;
+    var password = document.getElementById("createpass").value;
+    var password2 = document.getElementById("createpass2").value;
+
+    if(password != password2)
+    {
+        console.log("Error: Passwords do not match")
+    }
+    else
+    {
+        createUSER(first, last, username, email, password);
+        console.log("Created user " + username)
+        clearCreate();
+        console.log("Cleared Field Data")
+        $("#createmodal").modal('toggle');
+    }
+}
+
+function changeData(param)
+{
+    var user = "Chandan";
+    var database = firebase.database();
+
+    database.ref().once('child_added', function(snapshot)
+    {
+                var content;
+                content += "<tr>";
+                content += "<td>" + user.DateLoaned[0] + "</td>";
+                content += "<td>" + user.Paid+ "</td>";
+                content += "<td>" + user.DatePaid + "</td>";
+                content += "<td>" + user.LoanAMT+ "</td>";
+                content += "<td>" + user.LoanFrom + "</td>";
+                content += "<td>" + user.Notes + "</td>";
+                content += "</tr>";
+
+            $("#bodytable").append(content);
+            document.getElementById("currentBAL").innerHTML = user.totalDebt;
+            document.getElementById("currentDEBT").innerHTML = user.totalBalence;
+            document.getElementById("currentCREDIT").innerHTML = user.totalcred;
+
+    });
+    };
+
+function clearLogin()
+{
   document.getElementById("username1").value = "";
   document.getElementById("password").value = "";
-
-}
-function createUSER(){
-
 }
 
-
-function changeData(param){
-var user = param;
-var refrence = firebase.database().ref().child("USERHIST/" + user);
-rootRef.on("child_added", snap => {
-var                         });
-
-/*var user = "Chandan";
-
-var database = firebase.database();
-database.ref().once('child_added', function(snapshot){
-
-            var content;
-            content +="<tr>";
-            content += "<td>" + user.DateLoaned[0] + "</td>";
-            content += "<td>" + user.Paid+ "</td>";
-            content += "<td>" + user.DatePaid + "</td>";
-            content += "<td>" + user.LoanAMT+ "</td>";
-            content += "<td>" + user.LoanFrom + "</td>";
-            content += "<td>" + user.Notes + "</td>";
-            content += "</tr>";
-
-        $("#bodytable").append(content);
-        document.getElementById("currentBAL").innerHTML = user.totalDebt;
-        document.getElementById("currentDEBT").innerHTML = user.totalBalence;
-        document.getElementById("currentCREDIT").innerHTML = user.totalcred;
-
-});*/
-};
-
-function clearLogin(){
-  document.getElementById("username1").value = "";
-  document.getElementById("password").value = "";
-
-
-
-}
-function val() {
+function val()
+{
     var infos = snapshot
     var userdata = atob(infos.user);
     var passdata = atob(infos.pass);
     var username = document.getElementById("Username").value;
     var password = document.getElementById("Password").value;
     var currUser;
-    if (userdata.indexOf(username) != -1 && passdata.indexOf(password) != -1) {
-        if (userdata.indexOf(username) != 3 && passdata.indexOf(password) != 3) {
+    if (userdata.indexOf(username) != -1 && passdata.indexOf(password) != -1)
+    {
+        if (userdata.indexOf(username) != 3 && passdata.indexOf(password) != 3)
+        {
         changeData(username);
         location.assign("transHist.html")
         currUser = username;
         }
 
-        else if ((userdata.indexOf(username) == 3) && (passdata.indexOf(password) == 3)){
+        else if ((userdata.indexOf(username) == 3) && (passdata.indexOf(password) == 3))
+        {
 
           location.assign("adminPg.html")
 
         }
-        else{
+        else
+        {
 
           return
 
         }
     }
-
-
-
-    else {
+    else
+    {
         clearLogin();
-
-
-
-
 
     }
 }
