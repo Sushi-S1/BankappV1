36581cd89626d3c26c7e84c961eb6132714acb66
Sci-Phy <tristanlusardi@gmail.com>
2018-05-28 19:00:10 -0400

    Moved successful sign up banner into login modal
    Moved successful sign up banner into login modal
    Moved createion of user history from sign up stage as it is useless there
    Redeleted data2.json as we already have database.json (they have the same data anyway)
    Added a couple items to the history user display

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ $(function () {
 
 function validatedropdown() {
 
+    // This has no effect because the fields are empty.
+    // We need to determine what data will be going into the history
+    // I want: Timestamp request was submited, date loan was taken out, loan amount, location of transaction
+    firebase.database().ref('USERHIST/' + first).update({
+      DateLoaned: [],
+      // DatePaid: [],
+      // LoanFrom: [],
+      LoanAMT: [],
+      Notes: []
+    });
 }
 
 function signout() {
@@ -54,85 +64,72 @@ function clearCreate() {
 
 function createUSER(first, last, username, email, password) {
 
-
-
-
-
-
-    firebase.database().ref('UserINFO' ).update({
-        EMail: email,
+    firebase.database().ref('USERINFO/' + first).set({
+        Email: email,
         First: first,
         Last: last,
         Username: username,
         Password: password
     });
-    firebase.database().ref('USERDATA' ).update({
+    firebase.database().ref('USERDATA/' + first).set({
         CreditScore: "0.00",
         CurrentCredit: "0.00",
         CurrentDebt: "0.00"
     });
-    firebase.database().ref('USERHIST' ).update({
-      DateLoaned: [],
-      DatePaid: [],
-      LoanFrom: [],
-      LoanAMT: [],
-      Notes: []
-    });
-
-
-
 }
+
+
 
 
 function changeData(param){
 
-var user = "Eagle";
-var refrence = firebase.database().ref().child("USERDATA/" + user);
-refrence.on("child_added", snap => {
-  var credscore =  snap.child("CreditScore");
-  var currcred =  snap.child("CurrentCredit");
-  var currdebt =  snap.child("CurrentDebt");
+    var user = "Eagle";
+    var refrence = firebase.database().ref().child("USERDATA/" + user + "/");
 
-document.getElementById("currentCREDIT").innerHTML =   credscore;
-document.getElementById("currentBAL").innerHTML = "$" + currcred;
-document.getElementById("currentDEBT").innerHTML = "$" + currdebt;
+    refrence.on("child_added", snap => {
+        var credscore =  snap.child("CreditScore");
+        var currcred =  snap.child("CurrentCredit");
+        var currdebt =  snap.child("CurrentDebt");
+
+        document.getElementById("currentCREDIT").innerHTML =   credscore;
+        document.getElementById("currentBAL").innerHTML = "$" + currcred;
+        document.getElementById("currentDEBT").innerHTML = "$" + currdebt;
+    });
+
+    /*
+     var refrence2 = firebase.database().ref().child("USERHIST/" + user);
+     refrence2.on("child_added", snap => {
+       var dateLoan =  snap.child("CreditScore");
+       var datePay =  snap.child("CurrentCredit");
+       var lender =  snap.child("CurrentDebt");
+     $("#currentCREDIT").append(credscore);
+     $("#currentBAL").append(currcred);
+     $("#currentDEBT").append(currdebt);
+     document.getElementById("currentCREDIT").innerHTML =   credscore;
+     document.getElementById("currentBAL").innerHTML = "$" + currcred;
+     document.getElementById("currentDEBT").innerHTML = "$" + currdebt;
 
 
- });
-/*
- var refrence2 = firebase.database().ref().child("USERHIST/" + user);
- refrence2.on("child_added", snap => {
-   var dateLoan =  snap.child("CreditScore");
-   var datePay =  snap.child("CurrentCredit");
-   var lender =  snap.child("CurrentDebt");
- $("#currentCREDIT").append(credscore);
- $("#currentBAL").append(currcred);
- $("#currentDEBT").append(currdebt);
- document.getElementById("currentCREDIT").innerHTML =   credscore;
- document.getElementById("currentBAL").innerHTML = "$" + currcred;
- document.getElementById("currentDEBT").innerHTML = "$" + currdebt;
+    });*/
+    /*var user = "Chandan";
 
+    var database = firebase.database();
+    database.ref().once('child_added', function(snapshot){
 
-});*/
-/*var user = "Chandan";
+                var content;
+                content +="<tr>";
+                content += "<td>" + user.DateLoaned[0] + "</td>";
+                content += "<td>" + user.Paid+ "</td>";
+                content += "<td>" + user.DatePaid + "</td>";
+                content += "<td>" + user.LoanAMT+ "</td>";
+                content += "<td>" + user.LoanFrom + "</td>";
+                content += "<td>" + user.Notes + "</td>";
+                content += "</tr>";
 
-var database = firebase.database();
-database.ref().once('child_added', function(snapshot){
-
-            var content;
-            content +="<tr>";
-            content += "<td>" + user.DateLoaned[0] + "</td>";
-            content += "<td>" + user.Paid+ "</td>";
-            content += "<td>" + user.DatePaid + "</td>";
-            content += "<td>" + user.LoanAMT+ "</td>";
-            content += "<td>" + user.LoanFrom + "</td>";
-            content += "<td>" + user.Notes + "</td>";
-            content += "</tr>";
-
-        $("#bodytable").append(content);
-        document.getElementById("currentBAL").innerHTML = user.totalDebt;
-        document.getElementById("currentDEBT").innerHTML = user.totalBalence;
-        document.getElementById("currentCREDIT").innerHTML = user.totalcred;*/
+            $("#bodytable").append(content);
+            document.getElementById("currentBAL").innerHTML = user.totalDebt;
+            document.getElementById("currentDEBT").innerHTML = user.totalBalence;
+            document.getElementById("currentCREDIT").innerHTML = user.totalcred;*/
 };
 
 $(document).ready(function(){
@@ -149,16 +146,17 @@ function SignUp() {
     var email = document.getElementById("createemail").value;
     var password = document.getElementById("createpass").value;
     var password2 = document.getElementById("createpass2").value;
-    if(first == "" || last == "" || username == "" || email == "" || password == ""){
-    document.getElementById("fillout").innerHTML =  "Please fill out all the fields."
-    }
-    else if (password != password2) {
+
+    if(first == "" || last == "" || username == "" || email == "" || password == "" || password2 == ""){
+        document.getElementById("fillout").innerHTML =  "Please fill out all the fields."
+    } else if (password != password2) {
         document.getElementById("passdontmatch").innerHTML =  "Passwords don't match!"
     } else {
-      createUSER(first, last, username, email, password);
+        createUSER(first, last, username, email, password);
         $("#sucsessfully").show();
         clearCreate();
         $('#createmodal').modal('hide');
+        $('#fillout').modal('hide');
         loginOpen();
         //$("#TOS").modal();
     }
