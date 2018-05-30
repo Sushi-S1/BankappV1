26492d3c1492bfb0910f3ec1454c3049b19884f2
Sushi-S1 <sushisri10@gmail.com>
2018-05-29 20:16:42 -0400

    Started to fix the login function

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ var config = {
 };
 firebase.initializeApp(config);
 
-$(function () {
-  $('[data-toggle="popover"]').popover()
+$(function() {
+    $('[data-toggle="popover"]').popover()
 })
 
 
@@ -20,11 +20,11 @@ function validatedropdown() {
     // We need to determine what data will be going into the history
     // I want: Timestamp request was submited, date loan was taken out, loan amount, location of transaction
     firebase.database().ref('USERHIST/' + first).update({
-      DateLoaned: [],
-      // DatePaid: [],
-      // LoanFrom: [],
-      LoanAMT: [],
-      Notes: []
+        DateLoaned: [],
+        // DatePaid: [],
+        // LoanFrom: [],
+        LoanAMT: [],
+        Notes: []
     });
 }
 
@@ -56,54 +56,51 @@ function createaccountopen() {
 function clearCreate() {
     document.getElementById("createnamefirst").value = "";
     document.getElementById("createnamelast").value = "";
-    document.getElementById("createusername").value = "";
+
     document.getElementById("createemail").value = "";
     document.getElementById("createpass").value = "";
     document.getElementById("createpass2").value = "";
 }
 
-function createUSER(first, last, username, email, password) {
+function createUSER(first, last, email, password) {
 
 
     firebase.database().ref('USERINFO/' + first).update({
         Email: email,
-
         First: first,
         Last: last,
-        Username: username,
         Password: password
     });
 
-    firebase.database().ref('USERDATA/' + first ).update({
+    firebase.database().ref('USERDATA/' + first).update({
 
         CreditScore: "0",
         CurrentCredit: "0.00",
         CurrentDebt: "0.00"
 
-  });
-  firebase.database().ref('USERHIST/' + first ).update({
-    DateLoaned: [""],
-     DatePaid: [""],
-     LoanFrom: [""],
-     LoanAMT: [""],
-     Notes: [""]
+    });
+    firebase.database().ref('USERHIST/' + first).update({
+        DateLoaned: [""],
+        DatePaid: [""],
+        LoanFrom: [""],
+        LoanAMT: [""],
+        Notes: [""]
 
-});
-
-
+    });
 
 
 
 
 };
-function changeData(param){
+
+function changeData(param) {
 
     var user = param;
     var refrencecred = firebase.database().ref().child("USERDATA/" + user + "/CreditScore/");
 
     refrencecred.on("value", snap => {
 
-        document.getElementById("currentCREDIT").innerHTML =   snap.val();
+        document.getElementById("currentCREDIT").innerHTML = snap.val();
 
     });
 
@@ -127,21 +124,7 @@ function changeData(param){
     });
 
 
-    /*
-     var refrence2 = firebase.database().ref().child("USERHIST/" + user);
-     refrence2.on("child_added", snap => {
-       var dateLoan =  snap.child("CreditScore");
-       var datePay =  snap.child("CurrentCredit");
-       var lender =  snap.child("CurrentDebt");
-     $("#currentCREDIT").append(credscore);
-     $("#currentBAL").append(currcred);
-     $("#currentDEBT").append(currdebt);
-     document.getElementById("currentCREDIT").innerHTML =   credscore;
-     document.getElementById("currentBAL").innerHTML = "$" + currcred;
-     document.getElementById("currentDEBT").innerHTML = "$" + currdebt;
 
-
-    });*/
     /*var user = "Chandan";
 
     var database = firebase.database();
@@ -163,8 +146,8 @@ function changeData(param){
             document.getElementById("currentCREDIT").innerHTML = user.totalcred;*/
 };
 
-$(document).ready(function(){
-    $(".close").click(function(){
+$(document).ready(function() {
+    $(".close").click(function() {
         $("#sucsessfully").alert("close");
     });
 });
@@ -173,17 +156,17 @@ $(document).ready(function(){
 function SignUp() {
     var first = document.getElementById("createnamefirst").value;
     var last = document.getElementById("createnamelast").value;
-    var username = document.getElementById("createusername").value;
+
     var email = document.getElementById("createemail").value;
     var password = document.getElementById("createpass").value;
     var password2 = document.getElementById("createpass2").value;
 
-    if(first == "" || last == "" || username == "" || email == "" || password == "" || password2 == ""){
-        document.getElementById("fillout").innerHTML =  "Please fill out all the fields."
+    if (first == "" || last == "" || email == "" || password == "" || password2 == "") {
+        document.getElementById("fillout").innerHTML = "Please fill out all the fields."
     } else if (password != password2) {
-        document.getElementById("passdontmatch").innerHTML =  "Passwords don't match!"
+        document.getElementById("passdontmatch").innerHTML = "Passwords don't match!"
     } else {
-        createUSER(first, last, username, email, password);
+        createUSER(first, last, email, password);
         $("#sucsessfully").show();
         clearCreate();
         $('#createmodal').modal('hide');
@@ -195,45 +178,57 @@ function SignUp() {
 
 
 
-function clearLogin()
-{
-  document.getElementById("username1").value = "";
-  document.getElementById("password").value = "";
+function clearLogin() {
+    document.getElementById("username1").value = "";
+    document.getElementById("password").value = "";
 }
 
 
 function val() {
-    var infos = snapshot
-    var userdata = atob(infos.user);
-    var passdata = atob(infos.pass);
-    var username = document.getElementById("Username").value;
-    var password = document.getElementById("Password").value;
-    var currUser;
-    if (userdata.indexOf(username) != -1 && passdata.indexOf(password) != -1)
-    {
-        if (userdata.indexOf(username) != 3 && passdata.indexOf(password) != 3)
-        {
-        changeData(username);
-        location.assign("transHist.html")
-        currUser = username;
+
+    var name = document.getElementById("username1").value;
+    var password = document.getElementById("password").value;
+    var firename;
+    var firepass;
+
+
+
+    var refrencename = firebase.database().ref().child("USERDATA/" + name + "/First");
+
+    refrencename.on("value", snap => {
+
+        firename = snap.val();
+
+    });
+
+    var refrencepass = firebase.database().ref().child("USERDATA/" + name + "/Password");
+
+    refrencepass.on("value", snap => {
+
+
+        firepass = snap.val();
+    });
+
+
+
+    if (((firename && firepass) !== undefined && (firename && firepass) !== null)) {
+        if ((name == firename && password == firepass)) {
+            location.assign("transHist.html")
+            changeData(firename);
+
+        } else {
+
+            clearLogin();
+            document.getElementById("wronglogin").innerHTML = "Incorrect name or password!"
+            console.log("1")
+            console.log(firename)
+            console.log(firepass)
         }
-
-        else if ((userdata.indexOf(username) == 3) && (passdata.indexOf(password) == 3))
-        {
-
-          location.assign("adminPg.html")
-
-        }
-        else
-        {
-
-          return
-
-        }
-    }
-    else
-    {
+    } else {
         clearLogin();
-
+        document.getElementById("wronglogin").innerHTML = "Incorrect name or password!"
+        console.log("2")
+        console.log(firename)
+        console.log(firepass)
     }
 }
